a38786898fd246d0a5f9d2a4bb94c5363bc31e54
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-01-01 12:36:23 +0530

    made function for making conv and conn blocks in model class

--- model/yolo.py
+++ model/yolo.py
@@ -30,36 +30,51 @@ class YOLOv1(nn.Module):
     Args:
         nn (_type_): _description_
     """
-    def __init__(self):
+    def __init__(self, split_size, num_boxes, num_classes):
         super().__init__()
         
-        # extracting information from yaml file
-        conv_architecture_config_path = Path.cwd()/"config"/"architecture.yaml"
-        with open(conv_architecture_config_path, 'r') as f:
+        self.conv_architecture_config_path = Path.cwd()/"config"/"architecture.yaml"
+        # convolution block
+        self.conv_block = nn.Sequential(*self.get_conv_block(self.conv_architecture_config_path))
+        # connecting block
+        self.conn_block = self.get_conn_block(split_size=split_size, num_boxes=num_boxes, num_classes=num_classes)
+
+    # extracting information from yaml file
+    def get_conv_block(self, config_path):
+        with open(config_path, 'r') as f:
             cfg = yaml.safe_load(f)
 
-        # building conv, maxpool layers from config file
-        layers = []
-        for layer in cfg["conv_architecture"]:
-            if layer["type"] == "conv2d":
-                layers.append(ConvBlock(in_channels = layer["args"]["in_channels"],
-                                        out_channels = layer["args"]["out_channels"],
-                                        kernel_size = layer["args"]["kernel_size"],
-                                        stride = layer["args"]["stride"],
-                                        padding = layer["args"]["padding"]))
-            elif layer["type"] == "maxpool":
-                layers.append(nn.MaxPool2d(kernel_size = layer["args"]["kernel_size"],
-                                           stride = layer["args"]["stride"]))
-            elif layer["type"] == "repetative_conv":
-                for i in range(layer["repetation"]):
-                    for j in layer["content"]:
-                        layers.append(ConvBlock(in_channels = j["args"]["in_channels"],
-                                                out_channels = j["args"]["out_channels"],
-                                                kernel_size = j["args"]["kernel_size"],
-                                                stride = j["args"]["stride"],
-                                                padding = j["args"]["padding"]))
-        self.conv_block = nn.Sequential(*layers)    
-
+            # building conv, maxpool layers from config file
+            layers = []
+            for layer in cfg["conv_architecture"]:
+                if layer["type"] == "conv2d":
+                    layers.append(ConvBlock(in_channels = layer["args"]["in_channels"],
+                                            out_channels = layer["args"]["out_channels"],
+                                            kernel_size = layer["args"]["kernel_size"],
+                                            stride = layer["args"]["stride"],
+                                            padding = layer["args"]["padding"]))
+                elif layer["type"] == "maxpool":
+                    layers.append(nn.MaxPool2d(kernel_size = layer["args"]["kernel_size"],
+                                            stride = layer["args"]["stride"]))
+                elif layer["type"] == "repetative_conv":
+                    for i in range(layer["repetation"]):
+                        for j in layer["content"]:
+                            layers.append(ConvBlock(in_channels = j["args"]["in_channels"],
+                                                    out_channels = j["args"]["out_channels"],
+                                                    kernel_size = j["args"]["kernel_size"],
+                                                    stride = j["args"]["stride"],
+                                                    padding = j["args"]["padding"]))
+        return layers
+    
+    def get_conn_block(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
+        conn_block = nn.Sequential(nn.Flatten(),
+                                        nn.Linear(1024*S*S, 4096),
+                                        nn.LeakyReLU(0.1),
+                                        nn.Linear(4096, S * S * (C + B * 5))
+                                        )
+        return conn_block
+        
     def forward(self, x: torch.Tensor):
-        return self.conv_block(x)
+        return self.conn_block(self.conv_block(x))
 
