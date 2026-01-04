be3b2c1c3179743cf433a7250e129c3b18f48919
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-01-04 16:04:31 +0530

    did minor changes in loss class just to support the change in split size

--- model/loss.py
+++ model/loss.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from util.util import intersection_over_union
 
 class YoloLoss(nn.Module):
-    def __init__(self, S = 7, B = 2, C = 20):
+    def __init__(self, S = 25, B = 2, C = 11):
         super().__init__()
         self.S = S
         self.B = B
@@ -17,8 +17,8 @@ class YoloLoss(nn.Module):
         # reshaping the prediction to S by S with (C+2B) features
         predictions = predictions.reshape(-1, self.S, self.S, self.C + 5*self.B)
 
-        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
-        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        iou_b1 = intersection_over_union(predictions[..., 12:16], target[..., 12:16])
+        iou_b2 = intersection_over_union(predictions[..., 17:21], target[..., 12:16])
         
         # this method is used to get which bbox has highestt intersection over union
         ious = torch.cat((iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)), dim = 0)
@@ -26,30 +26,30 @@ class YoloLoss(nn.Module):
         # choosen_bbox is a mask which get value 0 if bbox1 have higher iou, gets value 1 if bbox2 have higher iou
         max_iou, choosen_bbox = torch.max(ious, dim = 0)
 
-        exist_obj = target[..., 20:21]
+        exist_obj = target[..., 11:12]
 
         ## -------------------- ##
         ## Box coordinate loss  ##  
         ## -------------------- ##
  
         # exist_obj make sure that bbox is there only if object is there and following line gets best bbox from the two predicted bbox from the model    
-        bbox_predictions = exist_obj*(predictions[..., 21:25]*(1 - choosen_bbox) + predictions[..., 26:30]*(choosen_bbox))
-        bbox_targets = exist_obj*target[..., 21:25]
+        bbox_predictions = exist_obj*(predictions[..., 12:16]*(1 - choosen_bbox) + predictions[..., 17:21]*(choosen_bbox))
+        bbox_targets = exist_obj*target[..., 12:16]
 
         # updated the width and height with their square root as per the loss fn in Yolov1 paper
         bbox_predictions[..., 2:4] = torch.sign(bbox_predictions[..., 2:4]*torch.sqrt(torch.abs(bbox_predictions[...,2:4])))
         bbox_targets[..., 2:4] = torch.sqrt(bbox_targets[..., 2:4])
 
         # loss considering bbox paramters where object exist
-        box_loss = self.lambda_coord*self.mse(bbox_predictions[..., 0:4], target[..., 21:25])
+        box_loss = self.lambda_coord*self.mse(bbox_predictions[..., 0:4], bbox_targets[..., 0:4])
 
         ## ---------------------------------------- ##
         ##  object loss if there is object present  ##
         ## ---------------------------------------- ##
 
         # obj presence prediction of choosen bbox which ahs highest iou with target bbox where obj exist
-        obj_predictions = exist_obj*((1 - choosen_bbox)*predictions[..., 20:21] + choosen_bbox*predictions[..., 25:26])
-        obj_targets = exist_obj*(target[..., 20:21])
+        obj_predictions = exist_obj*((1 - choosen_bbox)*predictions[..., 11:12] + choosen_bbox*predictions[..., 16:17])
+        obj_targets = exist_obj*(target[..., 11:12])
 
         # loss of probability showing object present where object exist
         object_loss = self.mse(obj_predictions, obj_targets)
@@ -59,15 +59,15 @@ class YoloLoss(nn.Module):
         ## --------------------------------------------- ##
 
         # loss of probability showing object present where object does not exist
-        no_object_loss = self.lambda_noobj*self.mse((1 - exist_obj)*predictions[..., 20:21], (1 - exist_obj)*(target[..., 20:21]))
+        no_object_loss = self.lambda_noobj*self.mse((1 - exist_obj)*predictions[..., 11:12], (1 - exist_obj)*(target[..., 11:12]))
 
-        no_object_loss += self.lambda_noobj*self.mse((1 - exist_obj)*predictions[..., 25:26], (1 - exist_obj)*(target[..., 20:21]))
+        no_object_loss += self.lambda_noobj*self.mse((1 - exist_obj)*predictions[..., 16:17], (1 - exist_obj)*(target[..., 11:12]))
         
         ## ---------- ##
         ## class loss ##
         ## ---------- ##
 
-        class_loss = self.mse(exist_obj*predictions[..., :20], exist_obj*target[..., :20])
+        class_loss = self.mse(exist_obj*predictions[..., :11], exist_obj*target[..., :11])
 
 
         loss = (box_loss              # loss due to error in bbox parameters
